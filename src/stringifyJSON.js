163ea79b80b4c8076643f8e8d8be50e78c8ff294
cen19 <wilsoncen@gmail.com>
2017-05-13 21:59:10 -0700

    second commit : finished the assignment as per video

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,29 +1,25 @@
 var stringifyJSON = function(obj) {
-  if (obj === true) {
-    return 'true';
-  } else if (obj === null) {
-    return 'null';
-  } else if (obj === false) {
-    return 'false';
-  } else if (typeof obj === 'number') {
-    return obj.toString();
-  } else if (typeof obj === 'string') {
-    return '"' + obj + '"';
-  } else if (Array.isArray(obj) && obj.length === 0) {
-    return '[]';
-  } else if (Array.isArray(obj) && obj.length > 0) {
-    return '[' + transformElementsToString(obj) + ']';
+  if (Array.isArray(obj)) {
+    var results = [];
+    obj.forEach(function(element) {
+      results.push(stringifyJSON(element));
+    });
+    return '[' + results.join(',') + ']';
   }
-};
+  if (obj && typeof obj === 'object') {
+    var results = [];
 
-function transformElementsToString(array) {
-  var output = [];
-  array.forEach(function (element) {
-    if (typeof element === 'number') {
-      output.push('"' + element + '"');
-    } else if (typeof element === 'string') {
-      output.push('"' + element.toString() + '"');
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || obj[key] === undefined) {
+        continue;
+      }
+      results.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
     }
-  });
-  return output.join(', ');
-}
+
+    return '{' + results.join(',') + '}';
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  return '' + obj;
+};
