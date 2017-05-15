41b0adfed28410896ca253772df80d046fa760d3
cen19 <wilsoncen@gmail.com>
2017-05-14 20:36:26 -0700

    part2 completed with video

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,33 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var nodes = [];
+  // create an inner function
+  var searchNodes = function(node) {
+    // one line of pseudo code should map to one line of real code
+    // compare node's classname with className
+    var parts = node.className.split(' ');
+    if (parts.indexOf(className) >= 0) {
+
+      // if matched, save node
+      nodes.push(node);
+    }
+
+    // iterate over children
+    for (var i = 0; i < node.children.length; i++) {
+      // for each child, invoke searchNodes
+      searchNodes(node.children[i]);
+    }
+  };
+  searchNodes(document.body);
+
+  return nodes;
 };
+
+// function that traverses that HTML that finds all the elemnts in it that has the class name
+
+// learn to think of the HTML as a tree
+// when we encounter a tree like stucture, it makes sense to use recursion
+
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,29 +1,29 @@
 var stringifyJSON = function(obj) {
-  if (obj === true) {
-    return 'true';
-  } else if (obj === null) {
-    return 'null';
-  } else if (obj === false) {
-    return 'false';
-  } else if (typeof obj === 'number') {
-    return obj.toString();
-  } else if (typeof obj === 'string') {
-    return '"' + obj + '"';
-  } else if (Array.isArray(obj) && obj.length === 0) {
-    return '[]';
-  } else if (Array.isArray(obj) && obj.length > 0) {
-    return '[' + transformElementsToString(obj) + ']';
+  if (Array.isArray(obj)) {
+    var results = [];
+    // if (obj.length === 0) {
+    //   return '[]';
+    // }
+    obj.forEach(function (element) {
+      results.push(stringifyJSON(element));
+    });
+    return '[' + results.join(',') + ']';
   }
-};
-
-function transformElementsToString(array) {
-  var output = [];
-  array.forEach(function (element) {
-    if (typeof element === 'number') {
-      output.push('"' + element + '"');
-    } else if (typeof element === 'string') {
-      output.push('"' + element.toString() + '"');
+  if (typeof obj === 'object' && obj) {
+    var objectResults = [];
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || obj[key] === undefined) {
+        continue;
+      }
+      objectResults.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
     }
-  });
-  return output.join(', ');
+    return '{' + objectResults.join(',') + '}';
+  }
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else {
+    return '' + obj;
+  }
+
 }
